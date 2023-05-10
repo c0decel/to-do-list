1b664445ba38ffc54e38a5d66def5435390c84bb
c0decel <124975714+c0decel@users.noreply.github.com>
2023-05-09 19:12:02 -0700

    Add files via upload
    1.9 exercise

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function addItem() {
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if(inputValue === '') {
+        alert('write something!');
+    } else {
+        $('#list').append(li);
+    }
+
+    function crossOut() {
+        li.addClass('strike');
+    }
+
+    li.on('dblclick', crossOut);
+
+    let crossOutButton = $('<crossOutButton> x</crossOutButton>')
+    li.append(crossOutButton);
+
+    crossOutButton.on('click', deleteListItem);
+    function deleteListItem() {
+        li.addClass('delete');
+    }
+}
